e2506d2dd5a68a62bc6bac32322ced1d009d93c0
renwenlong <18813084898@163.com>
2026-04-21 22:49:02 +0800

    feat(wechat-about): add user agreement and privacy policy entries [A3] [wechat-submission-blockers]

--- wechat/pages/profile/about/index.js
+++ wechat/pages/profile/about/index.js
@@ -3,5 +3,13 @@ Page({
     version: '__APP_VERSION__',
     gitSha: '__GIT_SHA__',
     buildTime: '__BUILD_TIME__'
+  },
+
+  onUserAgreement: function () {
+    wx.navigateTo({ url: '/pages/legal/terms/index' })
+  },
+
+  onPrivacyPolicy: function () {
+    wx.navigateTo({ url: '/pages/legal/privacy/index' })
   }
 })
