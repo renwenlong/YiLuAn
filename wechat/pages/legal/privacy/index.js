b90cf67ed696a62a668d56c332fe1a109e1eb7fa
renwenlong <18813084898@163.com>
2026-04-21 16:27:25 +0800

    feat(wechat): bind privacy policy last-updated date dynamically [A21-06]

--- wechat/pages/legal/privacy/index.js
+++ wechat/pages/legal/privacy/index.js
@@ -1,7 +1,12 @@
+// A21-06: \u9690\u79c1\u534f\u8bae\u201c\u6700\u8fd1\u66f4\u65b0\u65f6\u95f4\u201d\u52a8\u6001\u7ed1\u5b9a\uff0c\u6743\u5a01\u6e90\uff1aconfig/privacy.js
+const { PRIVACY_UPDATED_AT, PRIVACY_EFFECTIVE_AT } = require('../../../config/privacy')
+
 Page({
   data: {
     showBackTop: false,
-    scrollToTop: false
+    scrollToTop: false,
+    updatedAt: PRIVACY_UPDATED_AT,
+    effectiveAt: PRIVACY_EFFECTIVE_AT
   },
 
   onPageScroll: function (e) {
